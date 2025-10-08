924a6f410b264115ac7e2e655eca1083bf79172b
Ra <ramilpowers@gmail.com>
2025-10-09 02:14:59 +0300

    Made .ftl + DeveloperFilter. Implemented database. Made SignUpServlet. doPut, doDelete in HelloServlet

---
+++ Shakirov_11-400_hw1/src/main/java/ru/kpfu/itis/shakirov/http/UserForHttp.java
@@ -0,0 +1,58 @@
+package ru.kpfu.itis.shakirov.http;
+
+public class User {
+    private int id;
+    private String name;
+    private String gender;
+    private String email;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public User(int id, String name, String gender, String email, String status) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.status = status;
+    }
+    public User () {}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
